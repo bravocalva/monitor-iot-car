0c4ebf2f7728c09bde88fdd8bb5bd73c785ec924
bravocalva <168801188+bravocalva@users.noreply.github.com>
2024-05-16 11:47:52 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,37 @@
+//alert("Hola Mundo");
+
+let mensaje = document.getElementById("respuesta");
+
+function callApiRequest() {
+
+    // Hacer una petición para un usuario con ID especifico
+    axios.get("http://18.223.116.24/iot-car-control/back-end/apis/getRegistro.php")
+        .then(function (response) {
+            // manejar respuesta exitosa
+            console.log(response);
+            let respuestaServidor = 
+            response.data =="F"
+            ? "ADELANTE"
+            : response.data == "B"
+            ? "ATRAS"
+            : response.data == "L"
+            ? "IZQUIERDA"
+            : response.data == "R"
+            ? "DERECHA"
+            : "DETENER";
+            
+            respuesta.innerHTML = "Ultima respuesta: <strong> " + respuestaServidor;
+        })
+        .catch(function (error) {
+            // manejar error
+            console.log(error);
+        })
+        .finally(function () {
+            // siempre sera executado
+        });
+
+}
+
+setInterval(callApiRequest, 2000);
+
+
